efc85f3fafd8d0f73763ff595bfb2ef9b25b06e2
Maxim Varshavsky <code4rizz@gmail.com>
2025-05-29 15:54:13 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Grade Changer 2
---
+++ grade-changer-two.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name        Grade Changer 2
+// @namespace   GitHub
+// @match       *://*.powerschool.*/guardian/scores.html*
+// @grant       GM_setValue
+// @version     1.0
+// @author      genZrizzCode
+// @description Changes all grades lower than an A-, to an A (91%). Only works for the overview at scores.html. Grade Changer 1 will work on the overview.
+// @updateURL   https://raw.githubusercontent.com/genZrizzCode/Grade-Changer-2/main/grade-changer-two.user.js
+// ==/UserScript==
+
+// License: MIT License
+// Copyright (c) 2025 genZrizzCode
+
+(function() { // This makes the following code happen immediately
+    console.log("%cGrade Changer 2 init.", "font-size: 2em; font-weight: bold; color: #93F;") // %c makes it styleable
+    'use strict';
+    window.addEventListener('load', () => { // This makes the following code start when the page loads
+        let tbody = document.querySelector('table tbody'); // makes tbody = the first <tbody> on the webpage
+        if (tbody) { // If there is a <tbody>:
+            tbody.id = 'grade-table';  // Changes the <tbody>'s id to 'grade-table'
+            console.log('ID added to tbody:', tbody);
+
+            let badLetterGrades = ['B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F'].map(g => g.toUpperCase());
+            let replacementGrade = 'A \u00A0 91%'; // Use \u00A0 for non-breaking space
+
+            let cells = tbody.querySelectorAll('td'); // Selects all table cells in the <tbody>
+
+            cells.forEach(cell => { // Check each cell
+                let rawText = cell.textContent.replace(/\s+/g, '').toUpperCase();
+                let match = rawText.match(/^(A\+|A-|A|B\+|B-|B|C\+|C-|D\+|D-|D|F)/);
+                let gradeText = match ? match[0] : '';
+
+                if (badLetterGrades.includes(gradeText)) {
+                    console.log(`Fixing grade in <td>: ${gradeText} -> ${replacementGrade}`);
+                    cell.innerHTML = replacementGrade; // Use innerHTML to allow the non-breaking space
+                }
+            });
+        } else {
+            console.log('No tbody found.');
+        }
+        console.log("%cGrade Changer 2 fin.", "font-size: 2em; font-weight: bold; color: #93F;")
+    });
+})();
